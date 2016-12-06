31c90b13eb6d9defe0d6bf4cd49771b0f88f897c
zuosong <zu.so@163.com>
2016-12-06 09:44:25 +0800

    add the code file of get urllist

---
+++ Gongsibao/crawl_save_urllist.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import cStringIO
+import formatter
+import urllib
+from htmllib import HTMLParser
+
+
+data = urllib.urlopen("http://www.gongsibao.com").read()
+parser = HTMLParser(formatter.AbstractFormatter(formatter.DumbWriter(cStringIO.StringIO())))
+parser.feed(data)
+parser.close()
+url_list= parser.anchorlist
+fl=open('E:\\Private Doc\\files\\test.txt','w')
+for i in url_list:
+    fl.write(i)
+    fl.write("\n")
+fl.close()
