890bf030b5c56e71d7b98bcd0f8cf5d7218e484c
zuosong <zu.so@163.com>
2016-12-07 15:40:51 +0800

    modified

--- Gongsibao/crawl_save_urllist.py
+++ Gongsibao/crawl_save_urllist.py
@@ -1,33 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#date 2016-12-7
+#author zuosong
+#fixed point :move the code into functions
 import cStringIO
 import formatter
 import urllib
 import urlparse
 from htmllib import HTMLParser
 
+def get_urls(url='http://www.gongsibao.com'):
+    data = urllib.urlopen(url).read()
+    parser = HTMLParser(formatter.AbstractFormatter(formatter.DumbWriter(cStringIO.StringIO())))
+    parser.feed(data)
+    parser.close()
+    url_list= parser.anchorlist
+    print 'get_urls is running~~'
+    return url_list
 
-data = urllib.urlopen("http://www.gongsibao.com/").read()
-parser = HTMLParser(formatter.AbstractFormatter(formatter.DumbWriter(cStringIO.StringIO())))
-parser.feed(data)
-parser.close()
-url_list= parser.anchorlist
-url_update=[]
-url_list=list(set(url_list))#排重
-for url in url_list:
-    parsed = urlparse.urlparse(url)
-    host = parsed.netloc.split('@')[-1].split(':')[0]
-    if host != 'www.gongsibao.com':
-        if host == '' and url.startswith('/'):
-            url = 'http://www.gongsibao.com'+url
-            url_update.append(url)
+def url_filter(url='http://www.gongsibao.com'):
+    print 'url_filter is running~~'
+    url_list=get_urls('http://www.gongsibao.com')
+    url_list=list(set(url_list))#排重
+    url_list.sort()#进行一次排序
+    url_update = []
+    print len(url_list)
+    for url in url_list:
+        parsed = urlparse.urlparse(url)
+        host = parsed.netloc.split('@')[-1].split(':')[0]
+        if host != 'www.gongsibao.com':
+            if host == '' and url.startswith('/'):
+                url = 'http://www.gongsibao.com'+url
+                url_update.append(url)
+                #print url
+            else:
+                url_list.remove(url)
+                #print url
         else:
-            url_list.remove(url)
-    else:
-        url_update.append(url)
+            url_update.append(url)
+            #print url
+    print '**********'
+    return url_update
 
-fl=open('E:\\Private Doc\\files\\test.txt','w')
-for i in url_update:
-    fl.write(i)
-    fl.write("\n")
-fl.close()
+def output_urllist(file='E:\\Private Doc\\files\\test.txt',url_list=[]):
+    fl=open(file,'w')
+    url_update = url_list
+    print len(url_update)
+    for i in url_update:
+        fl.write(i)
+        fl.write("\n")
+    fl.close()
+
+def main():
+    url_list=url_filter('http://www.gongsibao.com/')
+    print 'Running~~~~'
+    output_urllist('E:\\Private Doc\\files\\test.txt',url_list)
+
+if __name__ =='__main__':
+    main()
